b55175316e3eca7e117d64423f488c5d9e9e7271
Oscar Lee <oscarlee@Oscars-MacBook-Pro.local>
2019-11-03 23:32:48 -0500

    Updates

---
+++ precache-manifest.429589a0725d48832a854502d42cfaed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96868c198f598c5ac4d1b37a0057aea",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "611f7f863c1a788c95ac",
+    "url": "/portfolio/static/css/main.fc56d064.chunk.css"
+  },
+  {
+    "revision": "c540cb54bed0d16b4e81",
+    "url": "/portfolio/static/js/2.2aee63e2.chunk.js"
+  },
+  {
+    "revision": "611f7f863c1a788c95ac",
+    "url": "/portfolio/static/js/main.b60c0573.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9a287d22738b40c709d1e5b13a2b38cd.js"
+  "/portfolio/precache-manifest.429589a0725d48832a854502d42cfaed.js"
 );
 
 self.addEventListener('message', (event) => {
