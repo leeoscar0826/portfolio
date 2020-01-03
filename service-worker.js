80b9eb928fafff68d66838c1edde4488375a078c
Oscar Lee <oscarlee@Oscars-MBP.fios-router.home>
2020-01-03 16:31:36 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9b180335611d1db11f5ba5a8e370e442.js"
+  "/portfolio/precache-manifest.c8458881de8cd5767811face7b9888ac.js"
 );
 
 self.addEventListener('message', (event) => {
